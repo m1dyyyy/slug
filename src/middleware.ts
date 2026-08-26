cc031ba127e8acdadeebf0b5f584a56ac3e7d99b
m1dyyyy <142227657+m1dyyyy@users.noreply.github.com>
2026-08-26 22:10:27 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,1 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export default async function middleware(req: NextRequest) {
-  // Просто пропускаем все запросы дальше без каких-либо проверок и авторизаций
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: [
-    "/((?!api/|_next/|images/|docs/|_proxy/|_static|_vercel|[\\w-]+\\.\\w+).*)",
-    "/s/:slug*",
-  ],
-};
