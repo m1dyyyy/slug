4955d9c1b2ff6678226fea5a74709d2ad701db28
m1dyyyy <142227657+m1dyyyy@users.noreply.github.com>
2026-08-26 22:00:46 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,80 +1,10 @@
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
-
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-import {
-  DEFAULT_LOGIN_REDIRECT_URL,
-  apiAuthPrefix,
-  checkRoutesPrefix,
-  redirectRoutesPrefix,
-  authRoutes,
-  protectedRoutes,
-  publicRoutes,
-} from "./routes";
-
-const { auth } = NextAuth(authConfig);
-
-export default auth(async (req) => {
-  const { nextUrl } = req;
-
-  const isLoggedIn = !!req.auth;
-
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isCheckRoute = nextUrl.pathname.startsWith(checkRoutesPrefix);
-  const isRedirectRoute = nextUrl.pathname.startsWith(redirectRoutesPrefix);
-  const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-  const slugRoute = req.nextUrl.pathname.split("/").pop();
-
-  // ⚙️ Is Api Route:
-  if (isApiAuthRoute) {
-    return;
-  }
-
-  // ⚙️ Is Auth Route. First, check is authenticated:
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(
-        new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl),
-      );
-    }
-    return;
-  }
-
-  // ⚙️ If Slug contains ``c``, redirect to /check/:slug:
-  if (slugRoute?.endsWith("&c")) {
-    return NextResponse.redirect(
-      new URL(`/check/${slugRoute.replace("&c", "")}`, nextUrl),
-    );
-  }
-
-  // ⚙️ Protected routes. If not authenticated, redirect to /auth:
-  if (!isLoggedIn && isProtectedRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    return NextResponse.redirect(
-      new URL(`/auth?callbackUrl=${encodedCallbackUrl}`, nextUrl),
-    );
-  }
-
-  // ⚙️ Redirect using slug:
-  // If not public route and not protected route:
-  if (
-    !isPublicRoute &&
-    !isProtectedRoute &&
-    !isCheckRoute &&
-    !isRedirectRoute
-  ) {
-    return NextResponse.redirect(new URL(`/redirect/${slugRoute}`, nextUrl));
-  }
-  return;
-});
+export default async function middleware(req: NextRequest) {
+  // Просто пропускаем все запросы дальше без каких-либо проверок и авторизаций
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
